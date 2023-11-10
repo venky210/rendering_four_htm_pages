37a10b8eeb7b405a5deb4435808f28f908a3c854
venky210 <vitsl.20735a0210@gmail.com>
2023-11-10 11:40:30 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+# Create your views here.
+def venky(request):
+    return render(request,'venky.html')
+
+
+def uday(request):
+    return render(request,'uday.html')
+
+
+def nikhil(request):
+    return render(request,'nikhil.html')
+
+
+def bhuvi(request):
+    return render(request,'bhuvi.html')
